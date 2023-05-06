c0deab42f916e807b2cc56b6df94322c41d6fda0
yago-marques <yagovictormarques@gmail.com>
2023-05-06 13:01:44 -0300

    adding swaggwer docs

--- config/urls.py
+++ config/urls.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from django.urls import path, include
 from app.views import PostViewSet, UserViewSet
 from rest_framework import routers
+from rest_framework.schemas import get_schema_view
+from django.views.generic import TemplateView
+
 
 router = routers.DefaultRouter()
 router.register(r'posts', PostViewSet)
@@ -10,4 +13,13 @@ router.register(r'users', UserViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path('admin/', admin.site.urls),
+    path('openapi/', get_schema_view(
+        title='Django Application',
+        description='A simple Django REST API',
+        version='1.0.0',
+    ), name='openapi-schema'),
+    path('docs/', TemplateView.as_view(
+        template_name='documentation.html',
+        extra_context={'schema_url':'openapi-schema'}
+    ), name='swagger-ui'),
 ]
